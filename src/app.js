77be6243b5544c7877ba5123a38414698bb2a20e
Andrew White <white.d.andrew@gmail.com>
2023-02-08 00:14:16 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+function typeOutMessage(targetElementId, message, minTypingInterval, maxTypingInterval) {
+    let targetElement = document.getElementById(targetElementId);
+    let currentIndex = 0;
+    let innerHTML = "";
+
+    function type() {
+        if (currentIndex < message.length) {
+            if (message.charAt(currentIndex) === "\r\n" || message.charAt(currentIndex) === "\n") {
+                innerHTML += "\n[root@localhost]";
+            } else {
+                innerHTML += message.charAt(currentIndex);
+            }
+            targetElement.innerHTML = innerHTML + "▂";
+            currentIndex++;
+            setTimeout(type, Math.random() * (maxTypingInterval - minTypingInterval) + minTypingInterval);
+        }
+    }
+
+    setTimeout(type, Math.random() * (maxTypingInterval - minTypingInterval) + minTypingInterval);
+}
+
+message = `
+Booting Framework44...
+Loading kernel modules...
+Mounting file systems...
+Starting system logging service...
+Configuring network interfaces...
+Initializing hardware detection and evaluation...
+Scanning for available hardware components...
+Found hardware component 0: Capture system 0 [PCI bus address: 00:02.0]
+Found hardware component 1: Flow modulator [PCI bus address: 01:00.0]
+Found hardware component 2: Controller 0 [PCI bus address: 02:00.0]
+Found hardware component 3: Controller 1 [PCI bus address: 02:05.0]
+Warming system bus: 0...1..2...3
+Hardware component 3 (Controller 0) detected as most suitable for optimal performance.
+Setting Controller 0 as the primary hardware component...
+Starting optimization loop
+All required software components for Controller 0 have been installed.
+Welcome to Framework44!
+`
+
+// when page is ready (no jquery)
+document.addEventListener("DOMContentLoaded", function (event) {
+    typeOutMessage("terminal", message, 50, 100);
+});
